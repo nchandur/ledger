d8c79683a6493a077748272163ac68fd19695da1
nchandur <chandur.nischal@gmail.com>
2025-05-21 00:51:55 -0400

    fix the delete functionality ASAPgit add .git add .

--- api/groups.go
+++ api/groups.go
@@ -6,6 +6,7 @@ import (
 	"ledger/db"
 	"ledger/models"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -77,10 +78,85 @@ func GroupHandler(r *gin.Engine) {
 			return
 		}
 
-		ctx.HTML(http.StatusOK, "groups.html", gin.H{
-			"name": groupName,
-		})
+		collection = db.Client.Database("ledgers").Collection(groupName)
 
+		cur, err := collection.Find(ctx, bson.D{})
+
+		if err != nil {
+			ctx.HTML(http.StatusNoContent, "groups.html", gin.H{"error": err.Error()})
+			return
+		}
+
+		expenses := []models.Expense{}
+
+		for cur.Next(ctx) {
+			var expense models.Expense
+			cur.Decode(&expense)
+			expenses = append(expenses, expense)
+		}
+
+		ctx.HTML(http.StatusOK, "groups.html", gin.H{
+			"name":     groupName,
+			"expenses": expenses,
+		})
+	})
+
+	r.POST("/group", func(ctx *gin.Context) {
+		action := ctx.PostForm("_action")
+		groupName := ctx.PostForm("name")
+
+		group, err := crud.AccessGroup(groupName)
+
+		fmt.Println(groupName, action)
+
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		switch action {
+		case "add":
+			item := strings.TrimSpace(ctx.PostForm("item"))
+			priceTxt := strings.TrimSpace(ctx.PostForm("price"))
+			lent := strings.TrimSpace(ctx.PostForm("lent"))
+			involved := ctx.PostFormArray("participants")
+			splitType := ctx.PostForm("splitType")
+
+			price, err := strconv.ParseFloat(priceTxt, 64)
+
+			if err != nil {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+
+			expense := models.Expense{Item: item, Price: price, Lent: lent, Involved: involved, SplitType: splitType}
+
+			err = group.AddItem(expense)
+
+			if err != nil {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+
+		case "delete":
+			itemID := strings.TrimSpace(ctx.PostForm("id"))
+
+			id, err := strconv.Atoi(itemID)
+
+			if err != nil {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+
+			err = group.RemoveItemByID(id)
+
+			if err != nil {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+		}
+
+		ctx.Redirect(http.StatusSeeOther, fmt.Sprintf("/group?name=%s", groupName))
 	})
 
 }
